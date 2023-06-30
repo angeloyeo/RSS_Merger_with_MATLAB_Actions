993b1829a3c6da811a0bf8dc48f23f5746e7800c
Donghoon Yeo <angeloyeo@gmail.com>
2023-06-30 13:32:06 +0900

    changed RSS format

--- merge_RSS.m
+++ merge_RSS.m
@@ -1,3 +1,26 @@
+%% entry template
+template = struct;
+template.id = "";
+template.published = "";
+template.updated = "";
+template.link = struct;
+template.link.relAttribute = 'alternate';
+template.link.relAttribute = convertCharsToStrings( ...
+  template.link.relAttribute);
+template.link.typeAttribute = 'text/html';
+template.link.typeAttribute = convertCharsToStrings( ...
+  template.link.typeAttribute);
+template.link.hrefAttribute = "";
+template.title = "";
+template.content = "";
+template.author = struct;
+template.author.name = "";
+template.author.uri = "";
+template.category = struct;
+template.category(1).termAttribute = "";
+template.category(2).termAttribute = "";
+
+%%
 myStr = readlines("README.md");
 myStr(strcmp(myStr,"")) = [];
 startsWith(myStr, "*")
@@ -15,12 +38,21 @@ for i_topics = 1:n_topics
     buildDate = string(day(datetime("now"), 'shortname')) + ", " + ...
         datestr(datetime("now"), "dd mmm yyyy HH:MM:ss") + ...
         " +0000";
-    itemStruct = struct('title',[],'link',[],'pubDate',[],'description',[]);
-    channelStruct = struct('generator', "NFE/5.0", 'title', topics(i_topics),'link', "https://raw.githubusercontent.com/angeloyeo/RSS_Merger_with_MATLAB_Actions/main/"+topics(i_topics)+".xml", ...
-        'language', "ko", 'webMaster', "angeloyeo@gmail.com", 'copyright', "Angelo Yeo", ...
-        'lastBuildDate', buildDate, 'description', topics(i_topics), 'item', itemStruct);
-    RSS_topics.(topics(i_topics)) = struct("xmlns_atomAttribute", "http://www.w3.org/2005/Atom", "versionAttribute", 2, ...
-        "channel", channelStruct);
+
+    linkStruct = struct('relAttribute', "alternate", 'typeAttribute', "text/html", 'hrefAttribute', []);
+    authorStruct = struct('name', [], 'uri', []);
+    categoryStruct = struct;
+    categoryStruct.termAttribute = ["cat1"; "cat2"];
+
+    entryStruct = struct('id', [], 'published', [], 'updated', [], 'link', linkStruct,...
+        'title',[], 'content', [], 'author', authorStruct, 'category', categoryStruct);
+
+    RSS_topics.(topics(i_topics)) = struct(...
+        'xmlInsAttribute', "http://www.w3.org/2005/Atom",...
+        'xml_langAttribute', "en-US",...
+        'title', topics(i_topics),...
+        'updated', buildDate,...
+        'entry', entryStruct);
 
     while(1)
         if startsWith(myStr(p_topics_find(i_topics) + addNum),"*")
@@ -28,15 +60,23 @@ for i_topics = 1:n_topics
             tempXML = readstruct(feedURL, "FileType", "xml");
 
             for i_item = 1:length([tempXML.channel.item.title])
-                RSS_topics.(topics(i_topics)).channel.item(end+1).title = tempXML.channel.item(i_item).title;
+
+
                 limitedDescription = limitWidth(extractHTMLText(tempXML.channel.item(i_item).description), 100) + "...";
-                RSS_topics.(topics(i_topics)).channel.item(end).description = limitedDescription;
 
                 pat = lettersPattern(3) + ", " + digitsPattern(2) + " " + lettersPattern(3) + " " + digitsPattern(4) + " " + digitsPattern(2) + ":" + digitsPattern(2) + ":" + digitsPattern(2);
                 temp_pubDate = extract(tempXML.channel.item(i_item).pubDate', pat);
                 temp_pubDate = datetime(temp_pubDate, "InputFormat", "eee, dd MMM yyyy HH:mm:ss", "Locale", "en_US", "TimeZone", "+0000");
-                RSS_topics.(topics(i_topics)).channel.item(end).pubDate = temp_pubDate;
-                RSS_topics.(topics(i_topics)).channel.item(end).link = tempXML.channel.item(i_item).link;
+                
+                template.id = " ";
+                template.published = datetime(temp_pubDate, 'format', "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'");
+                template.updated = template.published;
+                template.link.hrefAttribute = tempXML.channel.item(i_item).link;
+                template.title = tempXML.channel.item(i_item).title;
+                template.content= limitedDescription;
+                template.author.name = " ";
+                template.author.uri = " ";
+                RSS_topics.(topics(i_topics)).entry = [template, RSS_topics.(topics(i_topics)).entry];
             end
 
             addNum = addNum + 1;
@@ -48,22 +88,22 @@ for i_topics = 1:n_topics
             break;
         end
     end
-    RSS_topics.(topics(i_topics)).channel.item(1) = []
+    RSS_topics.(topics(i_topics)).entry(end) = []; % removing template
 end
 
 %% sort by date
 for i_topics = 1:n_topics
-    RSS_topics.(topics(i_topics)).channel.item = table2struct(sortrows(struct2table(RSS_topics.(topics(i_topics)).channel.item), "pubDate" ,"descend"));
+    RSS_topics.(topics(i_topics)).entry = ...
+        table2struct(...
+            sortrows(struct2table(RSS_topics.(topics(i_topics)).entry), "published" ,"descend"));
 end
 
 %% change dateformat back to original
 for i_topics = 1:n_topics
-    for i_entity = 1:length(RSS_topics.(topics(i_topics)).channel)
-        tempDate = RSS_topics.(topics(i_topics)).channel.item(i_entity).pubDate;
-        RSS_topics.(topics(i_topics)).channel.item(i_entity).pubDate = ...
-            string(day(tempDate, 'shortname'))' + ", " + ...
-            datestr(tempDate, "dd mmm yyyy HH:MM:ss") + ...
-            " +0000";
+    for i_entity = 1:length([RSS_topics.(topics(i_topics)).entry.published])
+        tempDate = RSS_topics.(topics(i_topics)).entry(i_entity).published;
+        RSS_topics.(topics(i_topics)).entry(i_entity).published = string(tempDate);
+        RSS_topics.(topics(i_topics)).entry(i_entity).updated = string(tempDate);
     end
 end
 
